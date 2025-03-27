d8c069689e2e8a92d973f700df3584626057a34a
Tiago Facco <thifacco@gmail.com>
2025-03-27 11:26:03 -0300

    Init

---
+++ src/main/java/med/voll/clinica_medica_spring_boot_api/ClinicaMedicaSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package med.voll.clinica_medica_spring_boot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaMedicaSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaMedicaSpringBootApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/med/voll/clinica_medica_spring_boot_api/ClinicaMedicaSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package med.voll.clinica_medica_spring_boot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaMedicaSpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
