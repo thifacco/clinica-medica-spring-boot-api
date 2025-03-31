b76982fdf7aa14ffbf71b005a70bc3073cb28824
Tiago Facco <thifacco@gmail.com>
2025-03-31 15:17:13 -0300

    Configuração de CORS

---
+++ src/main/java/med/voll/clinica_medica_spring_boot_api/config/CorsConfiguration.java
@@ -0,0 +1,15 @@
+package med.voll.clinica_medica_spring_boot_api.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
