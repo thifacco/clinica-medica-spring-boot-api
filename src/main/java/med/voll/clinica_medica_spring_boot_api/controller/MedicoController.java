ac9ad8ee9a9ca1cbed6c78e9798824b5ea904be4
Tiago Facco <thifacco@gmail.com>
2025-04-03 15:42:28 -0300

    Método GET listar médicos

--- src/main/java/med/voll/clinica_medica_spring_boot_api/controller/MedicoController.java
+++ src/main/java/med/voll/clinica_medica_spring_boot_api/controller/MedicoController.java
@@ -2,13 +2,13 @@ package med.voll.clinica_medica_spring_boot_api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.clinica_medica_spring_boot_api.medico.DadosCadastroMedico;
+import med.voll.clinica_medica_spring_boot_api.medico.DadosListagemMedico;
 import med.voll.clinica_medica_spring_boot_api.medico.Medico;
 import med.voll.clinica_medica_spring_boot_api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -20,5 +20,10 @@ public class MedicoController {
     @PostMapping
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         this.repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return this.repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
