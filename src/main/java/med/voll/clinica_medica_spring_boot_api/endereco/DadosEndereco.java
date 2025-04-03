0033746b823ec03c86fa0523b70c2fee23291f3b
Tiago Facco <thifacco@gmail.com>
2025-04-03 10:36:35 -0300

    Validação de dados com Jakarta Validation

--- src/main/java/med/voll/clinica_medica_spring_boot_api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/clinica_medica_spring_boot_api/endereco/DadosEndereco.java
@@ -1,11 +1,26 @@
 package med.voll.clinica_medica_spring_boot_api.endereco;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record DadosEndereco(
+        @NotBlank
         String logradouro,
+
+        @NotBlank
         String bairro,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
         String cep,
+
+        @NotBlank
         String cidade,
+
+        @NotBlank
         String uf,
+
         String complemento,
+
         String numero
 ) { }
