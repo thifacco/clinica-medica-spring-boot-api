493289418cb1654af1bca0dd640a9ceaaaf4d953
Tiago Facco <thifacco@gmail.com>
2025-04-03 23:10:30 -0300

    Paginação fixa com @PageableDefault

--- src/main/java/med/voll/clinica_medica_spring_boot_api/controller/MedicoController.java
+++ src/main/java/med/voll/clinica_medica_spring_boot_api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import med.voll.clinica_medica_spring_boot_api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,7 +26,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 2, sort = {"nome"}) Pageable paginacao) {
         return this.repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
